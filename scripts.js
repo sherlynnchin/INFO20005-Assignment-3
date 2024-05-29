e31926d3c1352056c09a232fdd5c6113748949d7
Sherlynn Chin <sherlynnyenc@student.unimelb.edu.au>
2024-05-29 18:16:48 +1000

    29th May

--- scripts.js
+++ scripts.js
@@ -96,45 +96,49 @@ function decreaseCount(element) {
     }
 }
 
-const button1 = document.querySelector(".addtocart");
-let done1 = null;
-if (button1) {
-    const done1 = button1.querySelector(".done");
-}
 
-const button2 = document.querySelector(".addtocartmobile");
-let done2 = null;
-if (button2) {
-    const done2 = button2.querySelector(".done");
-}
-console.log(button1, button2);
+document.addEventListener('DOMContentLoaded', () =>{
+    const button1 = document.querySelector(".addtocart");
+    let done1 = null;
 
-if (button1 && done1) {
-    let added1 = false;
-    button1.addEventListener('click', () => {
-        if (added1) {
-            done1.style.transform = "translate(-110%) skew(-40deg)";
-            added1 = false;
-        } else {
-            done1.style.transform = "translate(0px)";
-            added1 = true;
-        }
-    });
-}
+    if (button1) {
+        done1 = button1.querySelector(".done");
+    }
+    
+    const button2 = document.querySelector(".addtocartmobile");
+    let done2 = null;
+    if (button2) {
+        xdone2 = button2.querySelector(".done");
+    }
+    console.log(button1, button2);
+    
+    if (button1 && done1) {
+        let added1 = false;
+        button1.addEventListener('click', () => {
+            if (added1) {
+                done1.style.transform = "translate(-110%) skew(-40deg)";
+                added1 = false;
+            } else {
+                done1.style.transform = "translate(0px)";
+                added1 = true;
+            }
+        });
+    }
+    
+    if(button2 && done2) {
+        let added2 = false;
+        button2.addEventListener('click', () => {
+            if (added2) {
+                done2.style.transform = "translate(-110%) skew(-40deg)";
+                added2 = false;
+            } else {
+                done2.style.transform = "translate(0px)";
+                added2 = true;
+            }
+        });
+    }
 
-if(button2 && done2) {
-    let added2 = false;
-    button2.addEventListener('click', () => {
-        if (added2) {
-            done2.style.transform = "translate(-110%) skew(-40deg)";
-            added2 = false;
-        } else {
-            done2.style.transform = "translate(0px)";
-            added2 = true;
-        }
-    });
-}
-
+})
 
 
 function toggleDescription(descriptionId) {
@@ -172,7 +176,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 //Checkout button enabled
 const checkoutButton = document.getElementById('checkout-button');
-const deliveryButtons = document.querySelectorAll('input[name="selector"')
+const deliveryButtons = document.querySelectorAll('input[name="selector"]')
 
 deliveryButtons.forEach(radio => {
     radio.addEventListener('change', () => {
@@ -185,69 +189,75 @@ document.addEventListener('DOMContentLoaded', () =>{
     const proceedButton = document.querySelector('.checkout-button');
     const checkoutButton = document.getElementById ('checkoutForm');
 
+    if (proceedButton){
+        proceedButton.addEventListener('click', (event) => {
+            const firstName = document.getElementById('firstName');
+            const lastName = document.getElementById('lastName');
+            const phoneNumber = document.getElementById('phoneNumber');
+            const country = document.getElementById('country');
+            const postalCode = document.getElementById('postalCode');
+            const address1 = document.getElementById('address1');
+            const state = document.getElementById('state');
+            const city = document.getElementById('city');
+    
+            if(!firstName.value) {
+                firstName.setCustomValidity("Please enter your first name.");
+            } else {
+                firstName.setCustomValidity("");
+            }
+    
+            if(!lastName.value) {
+                lastName.setCustomValidity("Please enter your last name.");
+            } else {
+                lastName.setCustomValidity("");
+            }
+    
+            const phoneNumberPattern = /^\d*$/;
+    
+            if(!phoneNumber.value || !phoneNumberPattern.test(phoneNumber.value)) {
+                phoneNumber.setCustomValidity("Please enter a valid phone number.");
+            } else {
+                phoneNumber.setCustomValidity("");
+            }
+    
+            if(country.value === "placeholder") {
+                country.setCustomValidity("Please select a country.");
+            } else {
+                country.setCustomValidity("");
+            }
+    
+            const postalCodePattern = /^[0-9]{4}$/;
+    
+            if(!postalCode.value || !postalCodePattern.test(postalCode.value)) {
+                postalCode.setCustomValidity("Please enter a valid  4 digit postal code.");
+            } else {
+                postalCode.setCustomValidity("");
+            }
+    
+            if(!address1.value) {
+                address1.setCustomValidity("Please enter your address.");
+            } else {
+                address1.setCustomValidity("");
+            }
+    
+            if(!city.value) {
+                city.setCustomValidity("Please enter your city.");
+            } else {
+                city.setCustomValidity("");
+            }
+    
+            if (!checkoutForm.reportValidity()){
+                event.preventDefault();
+                event.stopPropagation();
+            }
+    
+        });
+
+
+    }
+
  
-    proceedButton.addEventListener('click', (event) => {
-        const firstName = document.getElementById('firstName');
-        const lastName = document.getElementById('lastName');
-        const phoneNumber = document.getElementById('phoneNumber');
-        const country = document.getElementById('country');
-        const postalCode = document.getElementById('postalCode');
-        const address1 = document.getElementById('address1');
-        const state = document.getElementById('state');
-        const city = document.getElementById('city');
-
-        if(!firstName.value) {
-            firstName.setCustomValidity("Please enter your first name.");
-        } else {
-            firstName.setCustomValidity("");
-        }
-
-        if(!lastName.value) {
-            lastName.setCustomValidity("Please enter your last name.");
-        } else {
-            lastName.setCustomValidity("");
-        }
-
-        const phoneNumberPattern = /^\d*$/;
-
-        if(!phoneNumber.value || !phoneNumberPattern.test(phoneNumber.value)) {
-            phoneNumber.setCustomValidity("Please enter a valid phone number.");
-        } else {
-            phoneNumber.setCustomValidity("");
-        }
-
-        if(country.value === "placeholder") {
-            country.setCustomValidity("Please select a country.");
-        } else {
-            country.setCustomValidity("");
-        }
-
-        const postalCodePattern = /^[0-9]{4}$/;
-
-        if(!postalCode.value || !postalCodePattern.test(postalCode.value)) {
-            postalCode.setCustomValidity("Please enter a valid  4 digit postal code.");
-        } else {
-            postalCode.setCustomValidity("");
-        }
-
-        if(!address1.value) {
-            address1.setCustomValidity("Please enter your address.");
-        } else {
-            address1.setCustomValidity("");
-        }
-
-        if(!city.value) {
-            city.setCustomValidity("Please enter your city.");
-        } else {
-            city.setCustomValidity("");
-        }
-
-        if (!checkoutForm.reportValidity()){
-            event.preventDefault();
-            event.stopPropagation();
-        }
-
-    });
+    
 
     
 })
